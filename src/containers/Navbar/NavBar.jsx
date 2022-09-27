70b326ee34aa2c568b889803a7b6398d2005d4f3
vineethraik <vineethraik23082000@gmail.com>
2022-09-27 15:07:41 +0530

    updating NavBar

--- src/containers/Navbar/NavBar.jsx
+++ src/containers/Navbar/NavBar.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import './NavBar.css'
 
-export const NavBar = () => {
+const NavLinks = ({route,changRoute}) => {
+    switch (route) {
+        case 'home':
+        case 'leaderboard':
+            //case 'profile':
+            return (
+                <div className="navBar navContainer">
+                    <p className="navBar navLink" onClick={() => {changRoute('home')}} >Home</p>
+                    <p className="navBar navLink" onClick={() => {changRoute('leaderboard')}} >LeaderBoard</p>
+                    <p className="navBar navLink" onClick={() => {changRoute('signout')}} >SignOut</p>
+                </div>
+            );
+        case 'signin':
+        case 'register':
+            return (
+                <div className="navBar navContainer">
+                    <p className="navBar navLink" onClick={() => {changRoute('signin') }} >SignIn</p>
+                    <p className="navBar navLink" onClick={() => { changRoute('register')}} >Register</p>
+                </div>
+            );
+        default:
+            break;
+    }
 
+}
+
+ const NavBar = ({ Route ,changeRoute}) => {
     return (
-        <div className="navbar">
-
+        <div className="navbar main">
+            <div className="navbar main1">Smart Brain</div>
+            <div className="navbar main2">
+                <NavLinks changRoute={changeRoute} route={Route} />
+            </div>
         </div>
     );
 }
+
+export default NavBar;
