4ab8127873c537c458e8115b5781f013ae313999
vineethraik <vineethraik23082000@gmail.com>
2022-11-29 23:29:54 +0530

    updating container rank

--- src/containers/Rank/Rank.jsx
+++ src/containers/Rank/Rank.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import './Rank.css'
 
 
-const Rank = () =>{
-    return ( 
-        <div>
-            you have requested n times
+const Rank = ({ user }) => {
+    const {name,entries}=user;
+    return (
+        <div className="rank main">
+            {
+                (user.entries.length === 0) ?
+                    <p className="rank text">{`Hello ${name} !, You haven't submitted any request yet`}</p>
+                    : (user.entries.length === 1) ?
+                        <p className="rank text">{`Hello ${name} !, You have submitted only one request till now`}</p>
+                        :
+                        <p className="rank text">{`Hello ${name} !, You have submitted ${entries.length} requests till now`}</p>
+            }
         </div>
     );
 }
