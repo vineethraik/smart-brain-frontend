1e56043fd2605f858a69a6f108df4a117d345733
vineethraik <vineethraik23082000@gmail.com>
2022-11-29 23:32:45 +0530

    updating project files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './controllers/App/App';
+// import Footer from './containers/Footer/Footer.jsx'
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
+    
   </React.StrictMode>
 );
 
